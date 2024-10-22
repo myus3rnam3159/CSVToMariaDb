fc42314b8121714646b583d883d5ebdced16b590
myus3rnam3159 <le3akmailg@gmail.com>
2024-10-22 17:00:31 +0700

    add mariadb connection params

--- script.py
+++ script.py
@@ -1,11 +1,19 @@
 import csv
+import mariadb
 
-def createTable(): 
+def createTables(): 
     data_sources = ["type_data.csv", "toscast_data.csv"]
     for source in data_sources:
         with open(source, 'r') as file:
             reader = csv.reader(file)
             headers = next(reader)
+conn_params = {
+    "user": "root",
+    "password": "root",
+    "host": "localhost",
+    "port": "3306",
+    "database": "equipment",
+}
 
 if __name__ == '__main__':
-    createTable()
+    createTables()
