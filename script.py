8a26191ecd8d98e8ba29c826eb982a212687371e
myus3rnam3159 <le3akmailg@gmail.com>
2024-10-24 15:15:05 +0700

    fix: connect to db and get table and column info

--- script.py
+++ script.py
@@ -1,19 +1,39 @@
 import csv
-import mariadb
 
-def createTables(): 
+from mariadb import Connection
+import mariadb
+from typing import List, Tuple
+
+def createConnection() -> Connection :
+    try:
+        conn_params = {
+            "user": "root",
+            "password": "root",
+            "host": "localhost",
+            "port": 3306,
+            "database": "equipment",
+        }
+        con = mariadb.connect(**conn_params)
+        print("Connected to mariadb successfully\n")
+        return con
+    except mariadb.Error as e:
+        print(f"Error connecting to the database: {e}\n")
+
+def getTableMetadata() -> List[Tuple[str, List[str]]]:
     data_sources = ["type_data.csv", "toscast_data.csv"]
+
+    result: List[Tuple[str, List[str]]] = []
     for source in data_sources:
+
         with open(source, 'r') as file:
             reader = csv.reader(file)
-            headers = next(reader)
-conn_params = {
-    "user": "root",
-    "password": "root",
-    "host": "localhost",
-    "port": "3306",
-    "database": "equipment",
-}
+
+            result.append(
+                (source.replace(".csv", ""), next(reader))
+            )
+    return result
 
 if __name__ == '__main__':
-    createTables()
+    metas = getTableMetadata()
+    print(metas)
+    conn = createConnection()
