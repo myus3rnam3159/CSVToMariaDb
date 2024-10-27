72edbfe7b332b8e1158d852689f09315176c7f5f
myus3rnam3159 <le3akmailg@gmail.com>
2024-10-27 16:56:14 +0700

    create trigger inserting data to result  after data inserted to type_data

--- trigger_insert.sql
+++ trigger_insert.sql
@@ -1,7 +1,9 @@
+DELIMITER |
 CREATE TRIGGER copy_to_result
 AFTER INSERT ON type_data
 FOR EACH ROW
 BEGIN
-    INSERT INTO result (equip_cd, otime, type_cd)
-    VALUES (NEW.equip_cd, NEW.otime, NEW.type_cd);
-END;
+    INSERT INTO result (OTIME, equip_cd, mode, count_no, ok, quality_ng, cool_ng, press, quality, out_trigger, shot_type)
+    VALUES (NEW.OTIME, NEW.equip_cd, NEW.mode, NEW.count_no, NEW.ok, NEW.quality_ng, NEW.cool_ng, NEW.press, NEW.quality, NEW.out_trigger, NEW.shot_type);
+END |
+DELIMITER ;
